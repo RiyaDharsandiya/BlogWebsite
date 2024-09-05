7f3dc0727ad289de4b541ae04fa5b133b5223101
Riya Kaushik Dharsandiya <riyadharsandiya@Riyas-MacBook-Air.local>
2024-09-05 19:05:23 +0530

    Initial commit

---
+++ public/js/firebase.js
@@ -0,0 +1,15 @@
+
+
+let firebaseConfig = {
+    apiKey: "AIzaSyBUkGWlQ-4cCSLFyuCDmLxBV2ya-G1GRms",
+    authDomain: "blogging-website-3636c.firebaseapp.com",
+    projectId: "blogging-website-3636c",
+    storageBucket: "blogging-website-3636c.appspot.com",
+    messagingSenderId: "16588832503",
+    appId: "1:16588832503:web:e037610e76950b6a8eddf2"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
